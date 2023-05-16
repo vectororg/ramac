2311746786887ee28a163e9045957b37a857fcad
Teemu Eskola <113065263+assariah@users.noreply.github.com>
2023-05-16 14:23:04 +0300

    some testing

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+
+    <footer className='footer'>
+      <div className='container-fluid-footer'>
+        <span>Hei Me lanitetaa</span>
+        <br></br>
+        <span>© 2023 Vectorama.fi</span>
+      </div>
+    </footer>
+  )
+}
