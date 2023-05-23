a5be686a134088493cc126bb1c61243bd6323025
Teemu Eskola <113065263+assariah@users.noreply.github.com>
2023-05-23 13:29:30 +0300

    Shopping Items

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,28 +5,36 @@ import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-
+import ShopItemGrid from '../components/ShopItemGrid'; // Import the ShopItemGrid component
 
 const MainPage = () => {
-    const { t } = useTranslation();
-  
-    return (
-      <div>
-        <Header />
-        <Navbar />
-        <Sidebar />
-     
-        <div className="main-container">
-          {/* Other components or navigation */}
-          <div>
-            <h1>{t('mainPage.title')}</h1>
-            <p>{t('mainPage.description')}</p>
-          </div>
+  const { t } = useTranslation();
+
+  const items = [
+    { id: 1, name: "Item 1", price: 10 },
+    { id: 2, name: "Item 2", price: 20 },
+    { id: 3, name: "Item 3", price: 30 },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Sidebar />
+
+      <div className="main-container">
+        {/* Other components or navigation */}
+        <div>
+          <h1>{t('mainPage.title')}</h1>
+          <p>{t('mainPage.description')}</p>
         </div>
-        <Footer />
+
+        {/* Include the ShopItemGrid component */}
+        <ShopItemGrid items={items} />
       </div>
-      
-    );
-  };
-  
-  export default MainPage;
+      <Footer />
+    </div>
+  );
+};
+
+export default MainPage;
