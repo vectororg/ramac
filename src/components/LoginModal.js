d1b8c7972eae9e6b968f2734c6b8e3e318f7297e
Teemu Eskola <113065263+assariah@users.noreply.github.com>
2023-05-24 13:25:47 +0300

    login form to bootstrap

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { Form, Button } from "react-bootstrap";
 
 function LoginWindow({ onClose }) {
-  const [formData, setFormData] = useState({ name: '', password: '' });
+  const [formData, setFormData] = useState({ name: "", password: "" });
   const { t } = useTranslation();
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
@@ -18,37 +20,52 @@ function LoginWindow({ onClose }) {
       .then((data) => {
         if (data.success) {
           // handle successful login
-          alert(t('loginSuccessful'));
+          alert(t("loginSuccessful"));
           console.log("Login successful");
         } else {
           // handle login error
-          alert(t('loginFailed'));
+          alert(t("loginFailed"));
           console.log(data.error);
         }
       })
       .catch((error) => console.error(error));
-      
   };
 
   return (
     <div className="login-window-wrapper">
       <div className="login-window">
         <div className="login-header">
-          <h2>{t('loginmodal.login')}</h2>
+          <h2>{t("loginmodal.login")}</h2>
           <button className="btn btn-outline-dark" onClick={onClose}>
             X
           </button>
         </div>
         <div className="login-body">
-          <form onSubmit={handleFormSubmit}>
-          <label>{t('loginmodal.username')}:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={(event) => setFormData({ ...formData, name: event.target.value })} />
-          <br />
-          <label>{t('loginmodal.password')}:</label>
-          <br />
-          <input type="password" id="password" name="password" value={formData.password} onChange={(event) => setFormData({ ...formData, password: event.target.value })} />
-            <button className="btn btn-outline-dark" type="submit">{t('loginmodal.login')}</button>
-          </form>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group controlId="name">
+              <Form.Label>{t("loginmodal.username")}:</Form.Label>
+              <Form.Control
+                type="text"
+                value={formData.name}
+                onChange={(event) =>
+                  setFormData({ ...formData, name: event.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>{t("loginmodal.password")}:</Form.Label>
+              <Form.Control
+                type="password"
+                value={formData.password}
+                onChange={(event) =>
+                  setFormData({ ...formData, password: event.target.value })
+                }
+              />
+            </Form.Group>
+            <Button variant="dark" type="submit">
+              {t("loginmodal.login")}
+            </Button>
+          </Form>
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,43 +3,41 @@ import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
 import LoginButton from '../components/LoginButton';
 import logo from '../img/logo.png';
+import cart from '../img/cart.png';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-
-
-
 
 const Navbar = () => {
   const { t } = useTranslation();
 
   return (
-    <>
     <nav className="navbar">
-    <div className="logo">
+      <div className="logo">
         <Link to="/">
           <img src={logo} alt="Logo" />
         </Link>
       </div>
       <Nav>
-      <Nav.Item>
-        <Nav.Link href="/hinnat">{t('navbar.hinnat')}</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-      <Nav.Link href="/info">{t('navbar.info')}</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-      <Nav.Link href="/tili">{t('navbar.tili')}</Nav.Link>
-      </Nav.Item>
-    </Nav>
+        <Nav.Item>
+          <Nav.Link href="/hinnat">{t('navbar.hinnat')}</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/info">{t('navbar.info')}</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/tili">{t('navbar.tili')}</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <ul>
         <LanguageSwitcher />
-     
         <li className='nav-item'>
-              <LoginButton />
-          </li>
+          <LoginButton />
+        </li>
+        <li className='cart'>
+          <img src={cart} alt="Cart" />
+        </li>
       </ul>
     </nav>
-    </>
   );
 };
 
