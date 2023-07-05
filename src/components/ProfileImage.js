e91f585830526f0dc9ccca2b020f733379e74106
Rauchwerfer <dmitrii.cherkasov.030298@gmail.com>
2023-07-05 16:09:28 +0300

    Some Refactoring on ProfileImage

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -6,24 +6,30 @@ export default function ProfileImage({ nick }) {
   const imgRef = useRef()
 
   useEffect(() => {
-    fetch('https://nr.vector.fi:1891/ramac/rest/v1/user/image?nick='+ nick, {
+
+    const fetchImage = async () => {
+      try {
+        const response = await fetch('https://nr.vector.fi:1891/ramac/rest/v1/user/image?nick='+ nick, {
           method: 'GET'
         })
-        .then(response => response.blob())
-        .then(data => {
+  
+        if (response.ok) {
+          const data = await response.blob();
+  
           const reader = new FileReader();
           reader.readAsDataURL(data); 
-
+  
           reader.onloadend = () => {
             const base64data = reader.result;                
-            //setImageBase64(base64data);
             imgRef.current.src = base64data;
           }
+        }
+      } catch (error) {
+        console.error(error)
+      }      
+    }
 
-          
-          
-        })
-        .catch(error => console.error(error));
+    fetchImage()
 
     return () => {
       
